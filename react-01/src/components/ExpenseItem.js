41396f1a9be4e3ecb833630d5a7c85d2ff5a2363
abhishek837 <iamabhi0803@gmail.com>
2021-07-16 15:26:57 +0530

    Splitting Component

--- react-01/src/components/ExpenseItem.js
+++ react-01/src/components/ExpenseItem.js
@@ -1,5 +1,26 @@
-function ExpenseItem(){
-    return <h2>Expense Item!</h2>
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    // const expenseDate=props.date//months starts from 0
+    // const expenseTitle=props.title;
+    // const expenseAmount=props.amount;
+    const month=props.date.toLocaleString('en-US',{month: 'long'});
+    const day=props.date.toLocaleString('en-US',{day: '2-digit'});
+    const year=props.date.getFullYear();
+    
+    return (
+        <div className="expense-item">
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
