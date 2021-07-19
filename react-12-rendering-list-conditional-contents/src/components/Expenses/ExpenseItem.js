7055afe9a40346511209edc00fe5075ecf13e59e
abhishek837 <iamabhi0803@gmail.com>
2021-07-19 21:01:57 +0530

    changes for list and conditional content part - 01

---
+++ react-12-rendering-list-conditional-contents/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Cards";
+
+const ExpenseItem = (props) => {
+  // const [title, setTitle]=useState(props.title);
+  // const clickHander = () => {
+  //     setTitle('upadted!');
+  // };
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        {
+          //<button onClick={clickHander}>Change Title</button>
+          //with state the title never chhanges
+        }
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
